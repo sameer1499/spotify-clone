ffdd8b69adf03559a18a9303e745001ea93299e9
sameer <sameer.sajeev@gmail.com>
2022-01-25 15:44:37 +0530

    Updates

---
+++ precache-manifest.a918da0f7c6354d6e87c89be6eab04a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddbc63c8a420635a234f562888377247",
+    "url": "/spotify-clone/index.html"
+  },
+  {
+    "revision": "0fbd25205566ec57a19c",
+    "url": "/spotify-clone/static/css/main.fb7fe8de.chunk.css"
+  },
+  {
+    "revision": "6f3ea09fc5bf85a6132a",
+    "url": "/spotify-clone/static/js/2.a652f748.chunk.js"
+  },
+  {
+    "revision": "277f9ac65ea629533c781a2e9dd27b69",
+    "url": "/spotify-clone/static/js/2.a652f748.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fbd25205566ec57a19c",
+    "url": "/spotify-clone/static/js/main.afd1ba64.chunk.js"
+  },
+  {
+    "revision": "52de21c6b549d6d1baa2",
+    "url": "/spotify-clone/static/js/runtime-main.f1339594.js"
+  }
+]);
